31194faabff6b8336eba4a78701435b07cf3afab
dk_vaja <vaja.dushyant11@gmail.com>
2021-05-08 12:05:04 +0530

    commit new branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavbarCont from './containers/NavbarCont';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <NavbarCont/>
+     <div className="counter-show"><span>0</span></div>
+     <div>
+       <button className="increment">+</button>
+     </div>
+    </>
   );
 }
 
